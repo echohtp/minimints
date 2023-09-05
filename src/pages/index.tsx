a076160b826ec86566ce900a6a597bb17494b966
banana <echohtp@gmail.com>
2023-09-05 17:19:20 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -125,26 +125,9 @@ export default function Home() {
                   </div>
                 </div>
 
-                <!-- 
-                <div>
-                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                    Count
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="number"
-                      name="count"
-                      id="count"
-                      value={count}
-                      onChange={(e) => {
-                        if (Number(e.target.value) > 0)
-                          setCount(Number(e.target.value))
-                      }}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-                -->
+                 
+
+                
 
                 <div className='py-2'>
                   <Button
